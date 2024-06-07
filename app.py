dacd15881ca0917123332f34826afa13c4cfe244
huey0905908327 <168401992+huey0905908327@users.noreply.github.com>
2024-06-07 22:58:40 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,37 +1,95 @@
 from flask import Flask, request, abort
-
-from linebot import (
-    LineBotApi, WebhookHandler
-)
-from linebot.exceptions import (
-    InvalidSignatureError
-)
-from linebot.models import *
-
-#======python的函數庫==========
-import tempfile, os
-import datetime
-import time
-import traceback
-#======python的函數庫==========
+from linebot import LineBotApi, WebhookHandler
+from linebot.exceptions import InvalidSignatureError
+from linebot.models import MessageEvent, TextMessage, TextSendMessage, MemberJoinedEvent
+import os
 
 app = Flask(__name__)
-static_tmp_path = os.path.join(os.path.dirname(__file__), 'static', 'tmp')
+
 # Channel Access Token
 line_bot_api = LineBotApi(os.getenv('CHANNEL_ACCESS_TOKEN'))
 # Channel Secret
 handler = WebhookHandler(os.getenv('CHANNEL_SECRET'))
 
+# 用户状态字典，用来存储每个用户的状态
+user_state = {}
+    
+questions_answers = {
+    '宮城縣': {
+        "日向 翔陽": "烏野高中10號",
+        "影山 飛雄": "烏野高中9號",
+        "澤村 大地": "烏野高中1號",
+        "菅原 孝支": "烏野高中2號",
+        "田中 龍之介": "烏野高中5號",
+        "東峰 旭": "烏野高中3號",
+        "西谷 夕": "烏野高中4號",
+        "月島 螢": "烏野高中11號",
+        "山口 忠": "烏野高中12號",
+        "緣下 力": "烏野高中6號",
+        "木下 久志": "烏野高中7號",
+        "成田 一仁": "成田一仁8號",
+        "及川 徹": "青葉城西高中1號",
+        "岩泉 一": "青葉城西高中4號",
+        "松川 一靜": "青葉城西高中2號",
+        "花卷 貴大": "青葉城西高中3號",
+        "渡親 治": "青葉城西高中7號",
+        "矢巾 秀": "青葉城西高中6號",
+        "金田一 勇太郎": "青葉城西高中12號",
+        "國見 英": "青葉城西高中13號",
+        "京谷 賢太郎": "青葉城西高中16號",
+        "茂庭 要": "伊達工業高中2號",
+        "青根 高伸": "伊達工業高中7號",
+        "二口 堅治": "伊達工業高中6號",
+        "作並 浩輔": "伊達工業高中13號",
+        "鐮先 靖志": "伊達工業高中1號",
+        "笹谷 武仁": "伊達工業高中3號",
+        "小原 豐": "伊達工業高中12號",
+        "黃金川 貫至": "伊達工業高中7號",
+        "女川 太郎": "伊達工業高中8號",
+        "牛島 若利": "白鳥澤學園高中1號",
+        "大平 獅音": "白鳥澤學園高中4號",
+        "天童 覺": "白鳥澤學園高中5號",
+        "五色 工": "白鳥澤學園高中8號",
+        "白布 賢二郎": "白鳥澤學園高中10號",
+        "川西 太一": "白鳥澤學園高中12號",
+        "山形 隼人": "白鳥澤學園高中14號",
+        "瀨見 英太": "白鳥澤學園高中3號",
+        "中島 猛": "和久谷南高中1號",
+        "川渡 瞬己": "和久谷南高中2號",
+        "鳴子 哲平": "和久谷南高中8號",
+        "白石 優希": "和久谷南高中4號",
+        "花山 一雅": "和久谷南高中5號",
+        "秋保 和光": "和久谷南高中10號",
+        "松島剛": "和久谷南高中11號",
+        "百澤 雄大": "角川學園高中9號",
+        "古牧 讓": "角川學園高中5號",
+        "南田 大志": "角川學園高中12號",
+        "淺蟲 快人": "角川學園高中4號",
+        "溫川 良明": "角川學園高中1號",
+        "馬門 英治": "角川學園高中3號",
+        "稲垣 功": "角川學園高中7號",
+        "照島 遊兒": "條善寺高中1號",
+        "二岐 丈春": "條善寺高中3號",
+        "沼尻 凜太郎": "條善寺高中7號",
+        "母畑 和馬": "條善寺高中2號",
+        "飯坂 信義": "條善寺高中9號",
+        "東山 勝道": "條善寺高中4號",
+        "土湯 新": "條善寺高中11號",
+        "池尻 隼人": "常波高中4號",
+        "十和田 良樹": "扇南高中4號",
+        "秋宮 昇": "扇南高中1號",
+        "唐松 拓巳": "扇南高中2號",
+        "田澤 裕樹": "扇南高中4號",
+        "森岳 步": "扇南高中5號",
+        "小安 颯真": "扇南高中10號",
+  }
+}
 
-# 監聽所有來自 /callback 的 Post Request
 @app.route("/callback", methods=['POST'])
 def callback():
-    # get X-Line-Signature header value
     signature = request.headers['X-Line-Signature']
-    # get request body as text
     body = request.get_data(as_text=True)
     app.logger.info("Request body: " + body)
-    # handle webhook body
     try:
         handler.handle(body, signature)
     except InvalidSignatureError:
@@ -39,64 +97,24 @@ def callback():
     return 'OK'
 
 
-# 處理訊息
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    msg = event.message.text
-    questions_answers = {
-        "apple": "蘋果",
-        "banana": "香蕉",
-        "cat": "貓",
-        "dog": "狗",
-        "elephant": "大象",
-        "flower": "花",
-        "guitar": "吉他",
-        "house": "房子",
-        "ice": "冰",
-        "tiger": "虎",
-        "jacket": "夾克",
-        "keyboard": "鍵盤",
-        "lemon": "檸檬",
-        "monkey": "猴子",
-        "notebook": "筆記本",
-        "orange": "橙子",
-        "piano": "鋼琴",
-        "queen": "女王",
-        "rabbit": "兔子",
-        "sun": "太陽",
-        "tree": "樹",
-        "umbrella": "雨傘",
-        "violin": "小提琴",
-        "whale": "鯨魚",
-        "xylophone": "木琴",
-        "yacht": "遊艇",
-        "zebra": "斑馬",
-        "bread": "麵包",
-        "car": "車",
-        "duck": "鴨子",
-        "你最近的感覺如何？": "我大多數時候感到焦慮和壓力很大。",
-        "你能描述一下你目前的心情嗎？": "我經常感到悲傷和沮喪。",
-        "你最近經歷過什麼重大生活變化嗎？": "是的，我最近搬到了一個新城市工作。",
-        "你通常如何應對壓力？": "我通常會試圖通過愛好或鍛煉來分散注意力，但這並不總是有效。",
-        "你有可以依靠的支持系統嗎？": "我有幾個可以談心的親密朋友和家人。",
-        "你喜歡做哪些活動？": "我喜歡閱讀、遠足和畫畫。",
-        "你有注意到你的睡眠模式有變化嗎？": "是的，我最近一直很難入睡和保持睡眠。",
-        "你對這次心理輔導有什麼目標？": "我希望學會更好地管理壓力，並提高我的整體幸福感。",
-        "還有什麼你認為我應該知道的嗎？": "我有時覺得自己不夠好，這影響了我的自信心。"
-    }
-    if msg in questions_answers:
-        #print(f"{english_word} 的中文翻譯是：{words_dict[english_word]}")
-    
-        line_bot_api.reply_message(event.reply_token, TextSendMessage(questions_answers[msg]))
+    user_id = event.source.user_id
+    msg = event.message.text.strip()
+
+    if user_id not in user_state:
+        user_state[user_id] = None
+
+    if msg == '宮城縣球員':
+        user_state[user_id] = '宮城縣'
+        line_bot_api.reply_message(event.reply_token, TextSendMessage("請輸入想查詢的球員名稱"))
     else:
-        line_bot_api.reply_message(event.reply_token, TextSendMessage(msg))
-       
-         
-
-@handler.add(PostbackEvent)
-def handle_message(event):
-    print(event.postback.data)
-
+        current_state = user_state[user_id]
+        if current_state and msg in questions_answers[current_state]:
+            reply = questions_answers[current_state][msg]
+            line_bot_api.reply_message(event.reply_token, TextSendMessage(reply))
+        else:
+            line_bot_api.reply_message(event.reply_token, TextSendMessage("未找到相關答案，請重新輸入相對應的關鍵字"))
 
 @handler.add(MemberJoinedEvent)
 def welcome(event):
@@ -106,9 +124,8 @@ def welcome(event):
     name = profile.display_name
     message = TextSendMessage(text=f'{name}歡迎加入')
     line_bot_api.reply_message(event.reply_token, message)
-        
-        
-import os
+
+
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
